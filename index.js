8495190c4d304a441cabe25b7900f744ff3f9562
Anderson Souza <andersonfebs@gmail.com>
2021-02-19 16:26:35 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const path = require('path')
+const convert = require('./lib/convert')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname,'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/cotacao', (req, res) => {
+  const { cotacao, quantidade } = req.query
+  const conversao = convert.convert(cotacao, quantidade)
+
+  if (cotacao && quantidade) {
+    res.render('cotacao', {
+      'error': false,
+      'cotacao': convert.toMoney(cotacao),
+      'quantidade': convert.toMoney(quantidade),
+      'conversao': convert.toMoney(conversao)
+    })
+  }else{
+    res.render('cotacao', {
+      error: 'Valores inválidos!'
+    })
+  }  
+})
+
+
+
+app.listen(5000, err => {
+  if (err) {
+    console.log('Não foi possível iniciar o servidor')
+  } else {
+    console.log('Servidor está online')
+  }
+})
