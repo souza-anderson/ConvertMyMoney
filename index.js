17f29470383e889726123d6799769a758cd4cb65
Anderson Souza <andersonfebs@gmail.com>
2021-02-23 18:17:58 -0400

    adding bcb lib

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const path = require('path')
 const convert = require('./lib/convert')
+const apiBCB = require('./lib/api-bcb')
 const app = express()
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname,'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.render('home')
+app.get('/', async (req, res) => {
+  const cotacao = await apiBCB.getCotacao()
+  res.render('home', {
+    cotacao
+  })
 })
 
 app.get('/cotacao', (req, res) => {
@@ -22,11 +26,11 @@ app.get('/cotacao', (req, res) => {
       'quantidade': convert.toMoney(quantidade),
       'conversao': convert.toMoney(conversao)
     })
-  }else{
+  } else {
     res.render('cotacao', {
       error: 'Valores inválidos!'
     })
-  }  
+  }
 })
 
 
